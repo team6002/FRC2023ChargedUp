adcf782c077e0b621f97982f563abf028bf51b04
Chris <cbnguyen3@gmail.com>
2023-03-05 18:51:08 -0500

    delete unused code in SUB_limelight

--- src/main/java/frc/robot/subsystems/SUB_LimeLight.java
+++ src/main/java/frc/robot/subsystems/SUB_LimeLight.java
@@ -15,54 +15,22 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.LimeLightConstants;
 
 public class SUB_LimeLight extends SubsystemBase {
-  // SUB_Blinkin m_blinkin;
-  double[] target1 = LimeLightConstants.kTarget1Constants;
-  double[] target2 = LimeLightConstants.kTarget2Constants;
-  double[] target3 = LimeLightConstants.kTarget3Constants;
-  double[] target4 = LimeLightConstants.kTarget4Constants;
-  double[] target5 = LimeLightConstants.kTarget5Constants;
-  double[] target6 = LimeLightConstants.kTarget6Constants;
-  double[] target7 = LimeLightConstants.kTarget7Constants;
-  double[] target8 = LimeLightConstants.kTarget8Constants;
-  SUB_FiniteStateMachine m_finiteStateMachine;
-  public SUB_LimeLight(
-    // SUB_Blinkin p_blinkin,
-    SUB_FiniteStateMachine p_finiteStateMachine) {
-    // m_blinkin = p_blinkin;
-    m_finiteStateMachine = p_finiteStateMachine;
-  }
+  public SUB_LimeLight() {}
+
   private double[] dv = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
   public double[] botpose;
-  public double wanted_x;
-  public double wanted_y;
-  public double wanted_z;
-  public double wanted_pitch;
-
-  public CommandBase exampleMethodCommand() {
-
-    return runOnce(
-        () -> {
-        });
-  }
 
   @Override
   public void periodic() {
-    // botpose =  NetworkTableInstance.getDefault().getTable("limelight").getEntry("botpose").getDoubleArray(dv);
     botpose = NetworkTableInstance.getDefault().getTable("limelight").getEntry("targetpose_robotspace").getDoubleArray(dv);
-    // if(m_finiteStateMachine.getState() == RobotState.SCORING){  
-    //   if(hasTarget()){
-    //     m_blinkin.setHasTarget();
-    //   }
-    //   else{
-    //     m_blinkin.setHasNoTarget();
-    //   }
-    // }
+
     telemetry();
   }
 
   public void telemetry(){
     SmartDashboard.putBoolean("DO YOU SEE ANYTHING ", hasTarget());
-    if(hasTarget()){
+
+    if (hasTarget()) {
       SmartDashboard.putNumber("target ID", getTargetID());
       SmartDashboard.putNumber("target x", getTargetX());
       SmartDashboard.putNumber("target Y", getTargetY());
@@ -73,10 +41,6 @@ public class SUB_LimeLight extends SubsystemBase {
     }
   }
 
-  @Override
-  public void simulationPeriodic() {
-  }
-
   public boolean hasTarget(){
     if(NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0) == 1){
       return true;
@@ -85,6 +49,7 @@ public class SUB_LimeLight extends SubsystemBase {
       return false;
     }
   }
+
   public double getTargetID(){
     return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tid").getDouble(0);
   }
@@ -99,18 +64,15 @@ public class SUB_LimeLight extends SubsystemBase {
   }
 
   public double getTargetX(){
-    // return Units.metersToInches(-botpose[2]);
     return -botpose[2];
   }
 
   public double getTargetY(){
-    // return Units.metersToInches(botpose[0]);
     return botpose[0];
   }
 
   public double getTargetZ(){
     return Units.metersToInches(botpose[1]);
-    // return botpose[1];
   }
 
   public double getTargetPitch(){
@@ -120,38 +82,4 @@ public class SUB_LimeLight extends SubsystemBase {
   public double getTargetYaw(){
     return -botpose[4];
   }
-
-  public double getWantedX(){
-    return wanted_x;
-  }
-
-  public double getWantedY(){
-    return wanted_y;
-  }
-  
-  public double getWantedZ(){
-    return wanted_z;
-  }
-
-  public double getWantedPitch(){
-    return wanted_pitch;
-  }
-
-  public void setWantedX(double p_wanted_x){
-     wanted_x = p_wanted_x;
-  }
-
-  public void setWantedY(double p_wanted_y){
-    wanted_y = p_wanted_y;
-  }
-  
-  public void setWantedZ(double p_wanted_z){
-    wanted_z = p_wanted_z;
-  }
-
-  public void setWantedPitch(double p_wanted_pitch){
-    wanted_pitch = p_wanted_pitch;
-  }
-
-  
 }
